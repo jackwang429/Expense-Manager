16d6fd0a8b6f66fa03556bf31cce864df1c02a29
Satyam Krishna <satyamkrishna92@gmail.com>
2014-10-08 02:01:21 +0530

    Foundation Setup

---
+++ web_app/index.php
@@ -0,0 +1,22 @@
+<?php
+
+?>
+
+
+<!doctype html>
+<html class="no-js" lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Foundation | Welcome</title>
+    <link rel="stylesheet" href="resources/css/foundation.css" />
+    <script src="resources/js/vendor/modernizr.js"></script>
+  </head>
+  <body>    
+    <script src="resources/js/vendor/jquery.js"></script>
+    <script src="resources/js/foundation.min.js"></script>
+    <script>
+      $(document).foundation();
+    </script>
+  </body>
+</html>
